008c3c0be5e73c1c2111dac4deaa0906d457f92e
Marko Gligić <32166373+maxencije@users.noreply.github.com>
2023-07-05 11:39:40 +0200

    commit

--- src/main/java/com/example/institucija/controller/InstitucijaController.java
+++ src/main/java/com/example/institucija/controller/InstitucijaController.java
@@ -1,16 +1,28 @@
 package com.example.institucija.controller;
 
+import com.example.institucija.domain.Institucija;
+import com.example.institucija.service.InstitucijaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/institucija")
 public class InstitucijaController {
 
-        @GetMapping
-        public ResponseEntity<String> hello() {
-            return ResponseEntity.ok("Hello World");
-        }
+    private InstitucijaService institucijaService;
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/{naziv}")
+    public ResponseEntity<Institucija> getInstitucija(@PathVariable String naziv) {
+        return ResponseEntity.ok().body(institucijaService.getInstitucija(naziv));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Institucija> addInstitucija(@RequestBody Institucija institucija) {
+        return ResponseEntity.ok().body(institucijaService.save(institucija));
+    }
 }
